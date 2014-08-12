28ce1bd68461f6c9d83f032343cf7717bb81c3f3
algobardo <mezzetti@gmail.com>
2014-08-12 14:43:40 +0200

    Fix

--- plugin/src/main/groovy/com/crashlytics/examples/gradle/PreprocessorPlugin.groovy
+++ plugin/src/main/groovy/com/crashlytics/examples/gradle/PreprocessorPlugin.groovy
@@ -2,43 +2,88 @@ package com.crashlytics.examples.gradle
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.tasks.Exec
+public class PreprocessorPlugin implements Plugin<Project> {
 
-class PreprocessorPlugin implements Plugin<Project> {
-
+/*
+  public class PreprocessorPluginExtension {
+      public PreprocessorPluginExtension(String base){
+        this.instrumentationPath = base;
+      }
+      String instrumentationPath
+  }
+*/
   void apply(Project project) {
+    //project.extensions.create("testInstrumentationConfig", PreprocessorPluginExtension, "nothing")
     project.configure(project) {
       if(it.hasProperty("android")) {
-        tasks.whenTaskAdded { theTask -> 
-          // Returns an empty list if the plugin only has the default flavor
-          // But we still need something to iterate over, so let’s make an empty flavor.
-          def projectFlavorNames = project.("android").productFlavors.collect { it.name }
-          projectFlavorNames = projectFlavorNames.size() != 0 ? projectFlavorNames : [""]
+            project.android.testVariants.all { variant ->
 
-          project.("android").buildTypes.all { build ->
-            def buildName = build.name
-            def flavorPath
-            for (flavorName in projectFlavorNames) {
-              if (!"".equals(flavorName)) {
-                flavorPath = "${flavorName}/${buildName}"
-              } else {
-                // If we are working with the empty flavor, there’s no second folder
-                flavorPath = "${buildName}"
+            String sootPath = "./soot/SootAndroidInstrumentation/bin/SootAndroidInstrumentation"
+
+            println sootPath;
+
+            logger.warn(variant.packageApplication.outputFile.toString())
+            logger.warn(variant.packageApplication.toString())
+            
+            variant.javaCompile << {
+              println "Working on " + variant.javaCompile.destinationDir.toString()  
+            }
+
+            def mytask = project.tasks.create("runtask${variant.baseName}",Exec.class)
+
+            mytask.configure {
+              dependsOn variant.javaCompile
+              doLast {
+                println 'Done instrumenting the tests'
               }
+              commandLine "${sootPath} ${variant.javaCompile.destinationDir}"
+            }
+
+            variant.dex.dependsOn mytask
+
+
+            /*
+            variant.javaCompile.doLast {
+              logger.warn("Done JAVA COMPILE.")
+            }
+            */
+            /*
+            // get output file
+            def outputFile = variant.outputFile
+
+            // add new signing task.
+            def signingTask = project.tasks.create("double${variant.name}Sign", MyCustomSigning)
+            // configure task
+            signingTask.inputFile = outputFile
+            // create the final apk name using baseName to guarantee it's unique.
+            signingTask.outputFile = new File(project.buildDir, "apks/${project.name}-${variant.baseName}-2sign.apk")
+
+            // configure the task to be run after the default signing task.
+            signingTask.dependsOn variant.packageApplication
+
+            // configure zip align task to use the output of the 2nd signing task,
+            // and also to run after 2nd signing task.
+            variant.zipAlign.inputFile = signingTask.outputFile
+            variant.zipAlign.dependsOn signingTask
+            */
+          }
+      }
+
+
+
+/*
+
+
+        tasks.whenTaskAdded { theTask -> 
+          
               def manifestPath = "build/manifests/${flavorPath}/AndroidManifest.xml"
-              def taskAffix
-              if (!"".equals(flavorName)) {
-                taskAffix = "${flavorName.capitalize()}${buildName.capitalize()}"
-              } else {
-                // If we are working with the empty flavor, there’s no second affix
-                taskAffix = "${buildName.capitalize()}"
-              }
-              def compileTask = "compile${taskAffix}".toString()
+              
+              def compileTask = "compileDebugJava".toString()
               if(compileTask.equals(theTask.name.toString())) {
-                def yourTaskName = "helloManifest${taskAffix}"
+                def yourTaskName = "myhelloworld"
                 project.task(yourTaskName) << {
-                  description = 'Outputs the manifest file size'
-                  def manifest = new File(manifestPath)
-                  logger.warn("***Hello World!*** Manifest Size: " + manifest.length())
+                  logger.warn("***Hello World!***")
                 }
                 theTask.dependsOn(yourTaskName)
                 def processTask = "process${taskAffix}Resources"
@@ -47,7 +92,8 @@ class PreprocessorPlugin implements Plugin<Project> {
             }
           }
         }
-      }
+*/
+          
     }
   }
 }
